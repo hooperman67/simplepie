63c9315806e73031cdcd7932d1d99220cc18e54d
hooperman67 <84323120+hooperman67@users.noreply.github.com>
2025-03-18 11:43:35 +0000

    Delete tests directory

--- tests/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-// require the composer autoload.php file
-require_once dirname(__DIR__) . '/vendor/autoload.php';
-
-require_once dirname(__DIR__) . '/autoloader.php';
